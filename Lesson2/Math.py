7a586b5311075fa0fb8c44ab936d4e6639b30871
PranavEakambaram <pranav.eakambaram.09@gmail.com>
2025-06-04 17:32:03 -0500

    Update Math.py

--- Lesson2/Math.py
+++ Lesson2/Math.py
@@ -1,1 +1,21 @@
 
+#Ex 1
+
+sandals_count = 3
+sneakers_count = 1
+boots_count = 2
+
+
+
+total_shoes = # Write code here
+
+
+
+
+#Ex 2
+
+quarters_per_dollar = 4
+number_of_dollars = 7.5
+
+number_of_quarters = number_of_dollars/.25
+print (num_of_quarters)
